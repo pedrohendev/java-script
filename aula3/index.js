36655be34ba18bfda539405eff268e723279c4f3
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-05-23 19:15:11 -0300

    javascript

---
+++ aula3/index.js
@@ -0,0 +1,20 @@
+// String = Texto 
+// Number = Número 
+
+console.log( 
+    "Luiz 'Otávio'" 
+);
+
+console.log( 
+    'Luiz "Otávio"'
+); 
+
+console.log(
+    `'Luiz' "Otávio"`
+);
+
+console.log(35, 15.85, 'Luis Otávio De Novo');
+
+console.log(
+    'Pedro Henrique da Silva Santos'
+);
