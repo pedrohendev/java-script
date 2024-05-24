2059de937165b29fb6f78cda66b30826fa6079bd
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-05-24 19:34:18 -0300

    novo

--- aula4/comentarios.js
+++ aula4/comentarios.js
@@ -1,5 +1,9 @@
-CSS(
-    body {
-        background-color: white;
-    };
-)
+/* Escrever um 
+comentário longo */
+console.log('Olá mundo'); // Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+// Comentário
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai se ignorado
