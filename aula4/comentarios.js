ecdc4993777194456d63e70d8e96906d082aadbf
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-05-26 16:22:15 -0300

    javascript

--- aula4/comentarios.js
+++ aula4/comentarios.js
@@ -7,5 +7,3 @@ console.log('Linha 2');
 console.log('Linha 3');
 console.log('Linha 4');
 // Vai se ignorado
-
-console.log)
---
+++ aula5/js/index.js
@@ -0,0 +1,5 @@
+//
+console.log('Olá mundo')
+console.log('Este trecho será exibido no console do navegador')
+alert('Olá mundo!') 
+alert('Meu nome é Pedro Henrique')
