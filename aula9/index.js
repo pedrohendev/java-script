173a0692baa4d0e14bd8ec4cdf078d388b75aece
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-05-28 18:50:47 -0300

    diferença entre var e let

---
+++ aula9/index.js
@@ -0,0 +1,23 @@
+// ECMAScript 2015 (ES6) JavaScript
+
+Nome = 'Pedro'; // NÃO FAÇA ISSO
+console.log(Nome);
+
+/* 
+let nOme = 'Luiz';
+let nOme = 'Otávio';
+console.log(nOme);
+NÃO SE UTILIZA ASSIM 
+*/ 
+
+let nOme = 'Luiz';
+nOme = 'Otávio';
+console.log(nOme);
+
+/*
+No 'var' podemos ultilizar
+*/
+
+var nome = 'Luiz';
+var nome = 'Otávio';
+console.log(nome);
