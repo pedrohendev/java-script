903180bc4d7dee3b576247d311693ec0b25ef49b
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-05-30 11:46:31 -0300

    novo

---
+++ exercicios/aula11/index.js
@@ -0,0 +1,16 @@
+/*
+Aritméticos:
+(+ Adição) / (Concatenação = unir dois valores (- / * ) )
+(- subtração) (/ divisão) (* multiplicação) (** potenciação) (% resto da divisão) 
+*/
+
+/*
+Ordem:
+(), **, * / %, + -
+*/
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log((num1 + num2) * num3);
+  
