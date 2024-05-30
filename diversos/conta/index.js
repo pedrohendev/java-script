c913659e756c91f0425fa22aef3f9387cd25eaca
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-05-30 12:41:13 -0300

    novo

---
+++ diversos/conta/index.js
@@ -0,0 +1,16 @@
+const salario = 1300;
+const faculdade = 285.26;
+const dizimo = 130;
+const denstista = 100;
+const notebook = 334.42;
+const cursoEmVideo = 25;
+const credito = 17;
+const tecladoMouseFoneSsd = 80.66;
+const presenteDiaNamorado = 170;
+const cartaoDeCredito = 80.95+7.50+21.64+11.6;
+
+const total = faculdade + dizimo + denstista + notebook + cursoEmVideo + credito + tecladoMouseFoneSsd + presenteDiaNamorado + cartaoDeCredito;
+
+console.log(salario - total);
+
+
--- exercicios/aula11/index.js
+++ exercicios/aula11/index.js
@@ -9,8 +9,41 @@ Ordem:
 (), **, * / %, + -
 */
 
+/*
+Incremento = ++
+Decremento = --
+*/
+
+/*
+Operadores de atribuição
+*/
+
 const num1 = 5;
 const num2 = 2;
 const num3 = 10;
 console.log((num1 + num2) * num3);
-  
+
+let contador = 1;
+++contador; // 2
+++contador; // 3
+++contador; // 4
+++contador; // 5
+console.log(contador)
+
+let Contador = 10;
+console.log(--Contador);
+
+const passo = 5;
+let cOntador = 0;
+cOntador += 2; // contador = contador + passo
+console.log(cOntador);
+
+//NaN = Not a number
+
+// parseInt(inteiro), parseFloat(decimal)
+
+const Num1 = 10;
+const Num2 = Number('Luiz');
+console.log(Num1 + Num2);
+console.log(typeof Num2)
+
