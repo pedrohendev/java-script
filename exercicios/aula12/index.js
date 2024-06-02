e048b543202bc2231f833de17e6df98773c1a094
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-06-02 17:20:33 -0300

    novo

--- exercicios/aula12/index.js
+++ exercicios/aula12/index.js
@@ -1,6 +1,8 @@
-const num1 = prompt('Digite um número');
-const num2 = prompt('Digite um número');
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um número');
+num1 = Number(num1);
+num2 = Number(num2);
 
 const resultado = num1 + num2;
 
-alert('O resultado foi:')
+alert(`O resulatado foi: ${resultado}`)
