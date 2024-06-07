546d023fb4752fb32fb32711adcd96db095b3711
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-06-07 20:57:29 -0300

    novo

--- diversos/conta/index.js
+++ diversos/conta/index.js
@@ -6,8 +6,9 @@ const notebook = 334.42;
 const cursoEmVideo = 25;
 const credito = 17;
 const tecladoMouseFoneSsd = 80.66;
-const presenteDiaNamorado = 100;
-const cartaoDeCredito = 80.95+7.50+21.64+11.6+30.80+26;
+const presenteDiaNamorado = 0;
+const cartaoDeCredito = 0;
+const rematriculaFaculdade = 0;
 
 const total = faculdade + dizimo + denstista + notebook + cursoEmVideo + credito + tecladoMouseFoneSsd + presenteDiaNamorado + cartaoDeCredito;
 
---
+++ diversos/qualquer coisa/index.js
@@ -0,0 +1,7 @@
+const dinheiro = 90;
+const cinema = 24;
+const comida = 45;
+
+const total = cinema + comida;
+
+console.log(dinheiro - total);
