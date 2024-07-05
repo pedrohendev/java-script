93483830c78bdbf133baf84130ac13ac9b3cc3db
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-05 13:58:05 -0300

    novo

---
+++ exercicios/aula19/index.js
@@ -0,0 +1,47 @@
+const raiz = n => n ** 0.5
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+
+/*const raiz = function (n) { // função dentro de uma variavel 
+    return n ** 0.5
+};
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))*/
+
+/*function soma(x = 1, y = 1) { // tem um valor padrão se não coloca algum valor
+    const resultado = x + y
+    return resultado
+}
+
+const resultado = soma(4, 2)
+console.log(resultado)*/
+
+
+/*function soma(x, y) {
+    const resultado = x + y
+    return resultado // tudo que está abaixo dele não é executado
+}
+
+const resultado = soma(2,2)
+console.log(resultado)*/
+
+/*function soma(x, y) {
+    const resultado = x + y
+    return resultado
+}
+
+console.log(soma(2, 2))
+console.log(soma(23, 54))
+console.log(soma(45432, 2432))*/
+
+
+/*function saudacao(nome) {
+    return `Bom dia! ${nome}` 
+}
+
+const variavel = saudacao(`Pedro`);
+console.log(variavel)*/
