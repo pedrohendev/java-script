d77300a235322de51ce72ff31ceba0433d718809
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-05 20:02:43 -0300

    novo

--- diversos/conta/index.js
+++ diversos/conta/index.js
@@ -6,7 +6,7 @@ let notebook = 334.42;
 let cursoEmVideo = 0;
 let credito = 17;
 let tecladoMouseFoneSsd = 80.66;
-let cartaoDeCredito = 254.6 + 33.41;
+let cartaoDeCredito = 254.6 + 33.41 + 29.90;
 let rematriculaFaculdade = 400;
 
 let total = faculdade + dizimo + denstista + notebook + cursoEmVideo + credito + tecladoMouseFoneSsd + cartaoDeCredito + rematriculaFaculdade;
