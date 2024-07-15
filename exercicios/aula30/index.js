a2e318a3d43e2358141266107ff4835b32c8d369
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-15 14:44:54 -0300

    novo

---
+++ exercicios/aula30/index.js
@@ -0,0 +1,39 @@
+const verdadeira = true;
+
+// let tem escopo de bloco { ... bloco }
+// var só tem escopo de função
+
+/*let nome = `luiz`; // criando
+var nome2 = `luiz`; // criando
+
+if (verdadeira) {
+    let nome = `otavio` // criando
+    var nome2 = `rogerio` // redeclarando
+
+    if (verdadeira) {
+        var nome2 = `ronaldo` // redeclarando
+        let nome = `outra coisa`       
+    }
+}
+
+console.log(nome, nome2)*/
+
+
+/*function falaOi() {
+    var sobrenome = `Miranda`
+
+    if (verdadeira) {
+    let nome = `Luiz`
+    console.log(sobrenome);
+    }
+
+    console.log(nome);
+}
+
+falaOi();*/
+
+
+console.log(sobrenome);
+
+let nome = `miranda`
+var sobrenome = `miranda`
