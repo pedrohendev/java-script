4b5ea4c5c24ec0d39757474b49d0a46a4ad9f11c
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-17 14:27:28 -0300

    novo

---
+++ exercicios/aula33 ex/assets/js/index.js
@@ -0,0 +1,15 @@
+const elementos = [
+    {tag: `P`, texto: `Frase 1`},
+    {tag: `div`, texto: `Frase 2`},
+    {tag: `footer`, texto: `Frase 3`},
+    {tag: `section`, texto: `Frase 4`},
+];
+
+
+function monstra(msg) {
+    msg.addEventListener(elementos[0,1,2,3])
+   
+}
+
+msg();
+
