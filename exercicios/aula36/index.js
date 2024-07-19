c092a1af80dd5db9b63b07f813b6794f37bbb6bf
Pedro <ph592848@gmail.com>
2024-07-19 20:32:08 -0300

    new commit

--- exercicios/aula36/index.js
+++ exercicios/aula36/index.js
@@ -1,3 +1,18 @@
 function random(min, max) {
-    
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r)
 }
+
+let rand = random(1, 50)
+console.log(rand)
+
+
+/*
+const nome = `Pedro`;
+let i = 0; 
+
+while (i <= nome.length) {
+    console.log(nome[i]);
+    i++
+}
+*/
