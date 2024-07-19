78d5ce4483e6decd7ed4c58ff7538c167898261d
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-19 13:29:41 -0300

    new commit

---
+++ exercicios/aula36/inde.js
@@ -0,0 +1,18 @@
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return r;
+}
+
+let rand = random(1, 50)
+console.log(rand)
+
+
+/*
+const nome = `Pedro`;
+let i = 0; 
+
+while (i <= nome.length) {
+    console.log(nome[i]);
+    i++
+}
+*/
