bb656d1b61d2d7e08c18302d5cb1e5dcf8215427
Pedro <ph592848@gmail.com>
2024-07-19 22:34:25 -0300

    otimizando o codigo no ex de logica de programção

--- exercicios/aula37 ex/index.js
+++ exercicios/aula37 ex/index.js
@@ -1,12 +1,6 @@
 // escreva uma função que recebe 2 números e retorne o maior deles
 
-function maiorValor( x, y ) {
-    if (x > y) {
-        return x;
-    } else {
-        return y;
-    }
-}
+const max2 = (x, y) => x > y ? x : y; 
 
-const maior = maiorValor( 2, 4 )
-console.log(maior)
+const maior = maiorValor(2, 4);
+console.log(maior);
