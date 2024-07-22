528e22e7b477aee933ecbac156f50a0cd2f3dbdc
Pedro <ph592848@gmail.com>
2024-07-22 10:54:57 -0300

    new commit

---
+++ exercicios/aula38/index.js
@@ -0,0 +1,22 @@
+function soma(x, y) {
+  if (typeof x !== `number` || typeof y !== `number`) {
+    throw new Error(`x e y precisam ser numeros`);
+  }
+
+  return x + y;
+}
+
+try {
+    console.log(soma(1, 2));
+    console.log(soma(`1`, 2))
+} catch(error) {
+    console.log(`Error!`)
+}
+
+/*
+try {
+    console.log(naoExisto);
+} catch(err) {
+    console.log(`naoExito não existe.`);
+}
+*/
